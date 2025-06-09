3e65013a901d255c2c6696dab41314317fdbfcf0
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2025-06-10 06:07:02 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
+USER_NAME = "onlyno999"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+PORT = 48888                   # Vmess端口，留空或0则自动生成
+DOMAIN = "argo.xxxxxxxx.nyc.mn"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYjIyYjY5YmE0MzdjYjI5ODRkNmQ3NGYwNmJhMjdiZDQiLCJ0IjoiZGI1MGMzNzUtY2Q0My00YjM5LTk0NTktM2FjNmM3NzlhODc3IiwicyI6IlpEaGlNV1l5WlRZdE9UZGxaaTAwT0RneExXSTFPVEV0TXpGalltTmlPV05oTnpWaCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
